0f08c0820a1ce5abe9ac041adda56081ebdc28ea
Game110AM <Game110AM@605-110-STU07>
2017-10-11 09:32:55 -0600

    #1 gravity

--- Oct 10-11-17/Declassified.cpp
+++ Oct 10-11-17/Declassified.cpp
@@ -1,8 +1,212 @@
 #include<iostream>
 #include<allegro5\allegro.h>
+#include<time.h>
+#include<stdlib.h>
 #include<allegro5\allegro_primitives.h>
+#include<stdio.h>
+
 using namespace std;
 
-int main() {
-	cout << "dead" << endl;
+int main(int argc, char **argv)
+{
+	ALLEGRO_DISPLAY *display = NULL;
+	ALLEGRO_EVENT_QUEUE *event_queue = NULL;
+	ALLEGRO_TIMER *timer = NULL;
+	ALLEGRO_BITMAP *square = NULL;
+
+
+
+	//these two variables hold the x and y positions of the square
+	//initalize these variables to where you want your square to start
+	float square_x = 50;
+	float square_y = 50;
+
+
+	bool isOnSolidGround = 0;
+
+	float velocity_vx = 0;
+	float velocity_vy = 0;
+
+
+	//here's our key states. they're all starting as "false" because nothing has been pressed yet.
+	//the first slot represents "up", then "down", "left" and "right"
+	bool key[4] = { false, false, false, false };
+
+
+	//don't redraw until an event happens
+	bool redraw = true;
+
+	//this controls our game loop
+	bool doexit = false;
+
+	al_init();
+
+	//get the keyboard ready to use
+	al_install_keyboard();
+
+	timer = al_create_timer(.02);
+
+	display = al_create_display(640, 480);
+
+	square = al_create_bitmap(32, 32);
+
+	al_set_target_bitmap(square);
+
+	al_clear_to_color(al_map_rgb(255, 255, 255));
+
+	al_set_target_bitmap(al_get_backbuffer(display));
+
+	event_queue = al_create_event_queue();
+
+	al_register_event_source(event_queue, al_get_display_event_source(display));
+
+	al_register_event_source(event_queue, al_get_timer_event_source(timer));
+
+	//tell the event queue that it should take keyboard events, too 
+	al_register_event_source(event_queue, al_get_keyboard_event_source());
+
+	al_clear_to_color(al_map_rgb(0, 0, 0));
+
+	al_flip_display();
+
+	al_start_timer(timer);
+
+
+
+
+	//so the game loop is set to act on "ticks" of the timer OR keyboard presses 
+	//OR the mouse closing the display
+	while (!doexit)
+	{
+		ALLEGRO_EVENT ev;
+		al_wait_for_event(event_queue, &ev);
+
+		//////////////////////////////////////////////////////////////////////////////////////////////////
+		//here's the movement algorithm
+
+		if (ev.type == ALLEGRO_EVENT_TIMER) {
+			//if the up button is pressed AND we're still below the top wall,
+			//move the box "up" by 4 pixels
+			if (isOnSolidGround) {
+				velocity_vx = .90;
+				if (key[0]) {
+					velocity_vy = -10;
+					isOnSolidGround == 0;
+				}
+			}
+			else {
+				isOnSolidGround == 1;
+			}
+			//if the left button is pressed AND we're still right of the left wall
+			//move the box left by 4 pixels
+			if (key[2] && square_x >= 0) {
+				velocity_vx = -4;
+			}
+
+			//if the left button is pressed AND we're still left of the right wall
+			//move the box right by 4 pixels
+			if (key[3] && square_x <= 640 - 32) {
+				velocity_vx = 4;
+			}
+			if (isOnSolidGround == 0) {
+				velocity_vy += .5;
+			}
+			if (square_y > 480 - 32) {
+				square_y = 480 - 32;
+				velocity_vy = 0;
+				isOnSolidGround = 1;
+			}
+			else {
+				isOnSolidGround = 0;
+			}
+			square_x += velocity_vx;
+			square_y += velocity_vy;
+
+			//redraw at every tick of the timer
+			redraw = true;
+		}
+		///////////////////////////////////////////////////////////////////////////////////////////////////////
+		else if (ev.type == ALLEGRO_EVENT_DISPLAY_CLOSE) {
+			break;
+		}
+
+		//////////////////////////////////////////////////////////////////////////////////////////////////////
+		//here's the algorithm that turns key presses into events
+		//a "key down" event is when a key is pushed
+		//while a "key up" event is when a key is released
+
+		//has something been pressed on the keyboard?
+		else if (ev.type == ALLEGRO_EVENT_KEY_DOWN/* ||
+												  ALLEGRO_EVENT_MOUSE_ENTER_DISPLAY*/) {
+			switch (ev.keyboard.keycode) {
+
+				//if the up key has been pressed
+			case ALLEGRO_KEY_UP:
+				key[0] = true;
+				break;
+
+				//if the down key has been pressed
+			case ALLEGRO_KEY_DOWN:
+				key[1] = true;
+				break;
+
+				//if the left key has been pressed
+			case ALLEGRO_KEY_LEFT:
+				key[2] = true;
+				break;
+
+				//if the right key has been pressed
+			case ALLEGRO_KEY_RIGHT:
+				key[3] = true;
+				break;
+			}
+		}
+		//has something been released on the keyboard?
+		else if (ev.type == ALLEGRO_EVENT_KEY_UP) {
+			switch (ev.keyboard.keycode) {
+			case ALLEGRO_KEY_UP:
+				key[0] = false;
+				break;
+
+			case ALLEGRO_KEY_DOWN:
+				key[1] = false;
+				break;
+
+			case ALLEGRO_KEY_LEFT:
+				key[2] = false;
+				break;
+
+			case ALLEGRO_KEY_RIGHT:
+				key[3] = false;
+				break;
+
+				//kill the program if someone presses escape
+				//case ALLEGRO_KEY_ESCAPE:
+				//ESC = true;
+				//break;
+			}
+		}
+
+		//if the clock ticked but no other events happened, don't bother redrawing
+		if (redraw && al_is_event_queue_empty(event_queue)) {
+			redraw = false;
+
+			//paint black over the old screen, so the old square dissapears
+			al_clear_to_color(al_map_rgb(0, 0, 0));
+
+			//the algorithm above just changes the x and y coordinates
+			//here's where the bitmap is actually drawn somewhere else
+			al_draw_bitmap(square, square_x, square_y, 0);
+			
+
+			al_flip_display();
+		}
+	}
+
+	al_destroy_bitmap(square);
+	al_destroy_timer(timer);
+	al_destroy_display(display);
+	al_destroy_event_queue(event_queue);
+
+	return 0;
 }
